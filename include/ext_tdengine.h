b8378f91e29861fc327bbd7ebe5e412a65cbe7d4
Yurun <admin@yurunsoft.com>
2022-01-15 21:14:58 +0800

    test

--- include/ext_tdengine.h
+++ include/ext_tdengine.h
@@ -11,7 +11,11 @@
 #include "Zend/zend_exceptions.h"
 #include "ext/spl/spl_exceptions.h"
 
-#define IS_WIN (defined(WIN32) || defined(_WIN32) || defined(__WIN32__) || defined(WIN64) || defined(_WIN64) || defined(__WIN64__) || defined(__NT__)) ? 1 : 0
+#if defined(WIN32) || defined(_WIN32) || defined(__WIN32__) || defined(WIN64) || defined(_WIN64) || defined(__WIN64__) || defined(__NT__)
+    #define IS_WIN 1
+#else
+    #define IS_WIN 0
+#endif
 
 // PHP < 8.0
 #ifndef RETURN_THROWS
