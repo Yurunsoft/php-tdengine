ee6ae52854097d66c13eff21c7b0934ba69f5cea
Yurun <admin@yurunsoft.com>
2022-01-15 13:33:32 +0800

    test

--- .github/script/build-tdengine.bat
+++ .github/script/build-tdengine.bat
@@ -9,6 +9,6 @@ cd release
 
 call "C:\Program Files (x86)\Microsoft Visual Studio\2019\Enterprise\VC\Auxiliary\Build\vcvarsall.bat" x64
 
-cmake .. -G "NMake Makefiles"
+cmake .. -G "NMake Makefiles" -DBUILD_JDBC=false -DTD_BUILD_HTTP=false -DTD_BUILD_LUA=false
 
 nmake install
