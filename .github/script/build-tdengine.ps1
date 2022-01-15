10e95e7884c94482188d34746576d65436f1f603
Yurun <admin@yurunsoft.com>
2022-01-15 12:13:03 +0800

    test

--- .github/script/build-tdengine.ps1
+++ .github/script/build-tdengine.ps1
@@ -4,16 +4,16 @@ cd D:\
 
 Invoke-Webrequest "https://github.com/taosdata/TDengine/archive/refs/tags/ver-$env:TDENGINE_VERSION.zip" -Outfile "tdengine.zip"
 
-Expand-Archive "tdengine.zip" "D:\tdengine"
+Expand-Archive "tdengine.zip" "D:\tdengine\"
 
-cd "D:\tdengine"
+cd "D:\tdengine\TDengine-ver-$env:TDENGINE_VERSION"
 
 mkdir release
 
 cd release
 
-&"C:\Program Files (x86)\Microsoft Visual Studio\2019\Enterprise\VC\Auxiliary\Build\vcvarsall.bat" x64
+cmd /c "C:\Program Files (x86)\Microsoft Visual Studio\2019\Enterprise\VC\Auxiliary\Build\vcvarsall.bat" x64
 
-cmake .. -G "NMake Makefiles"
+cmd /c cmake .. -G "NMake Makefiles"
 
-nmake
+cmd /c nmake install
