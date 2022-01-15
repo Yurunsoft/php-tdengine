2e71952d3473c9cbde6344e12205a587fe469e31
Yurun <admin@yurunsoft.com>
2022-01-15 13:12:44 +0800

    test

--- .github/script/build-tdengine.bat
+++ .github/script/build-tdengine.bat
@@ -7,7 +7,7 @@ mkdir release
 
 cd release
 
-"C:\Program Files (x86)\Microsoft Visual Studio\2019\Enterprise\VC\Auxiliary\Build\vcvarsall.bat" x64
+call "C:\Program Files (x86)\Microsoft Visual Studio\2019\Enterprise\VC\Auxiliary\Build\vcvarsall.bat" x64
 
 cmake .. -G "NMake Makefiles"
 
