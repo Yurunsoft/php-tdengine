72cc9191419c96eebd1d0615c794ba24180c2696
Yurun <admin@yurunsoft.com>
2022-01-17 16:22:56 +0800

    test

--- .github/script/build-tdengine-macos.sh
+++ .github/script/build-tdengine-macos.sh
@@ -4,5 +4,4 @@ wget https://github.com/taosdata/TDengine/archive/refs/tags/ver-${TDENGINE_VERSI
 mkdir -p tdengine && \
 tar -xf tdengine.tar.gz -C tdengine --strip-components=1 && \
 cd tdengine && \
-mkdir debug && cd debug && cmake .. && make && make install && \
-brew services start taosd
+mkdir debug && cd debug && cmake .. && make && make install
