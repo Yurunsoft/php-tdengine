57404c198a1d281a99c90a3500e1124c82b9a457
Yurun <admin@yurunsoft.com>
2022-08-26 10:10:14 +0800

    修复

--- .github/script/build-tdengine.sh
+++ .github/script/build-tdengine.sh
@@ -2,5 +2,5 @@
 cd /tmp && \
 git clone --recurse -b ver-${TDENGINE_VERSION} --depth=1 https://github.com/taosdata/TDengine.git && \
 cd TDengine && \
-mkdir release && cd release && cmake -DCMAKE_BUILD_TYPE=Release .. && cmake --build . && make install && \
+mkdir release && cd release && cmake -DCMAKE_BUILD_TYPE=Release .. && cmake -Werror=return-type --build . && make install && \
 systemctl start taosd
